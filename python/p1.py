b5fbbdaef8457bb5782b07fbe5d755723eb7c43d
kunaljoshi7661 <kunalcreationss1@gmail.com>
2025-08-04 22:57:37 +0530

    basics

--- python/p1.py
+++ python/p1.py
@@ -1,2 +1,8 @@
-print("hello world")
-print(3 +2 )
+line1 ='********************'  # header and footer
+line2 ='*                  *'  # reuse
+line3 ='*     Welcome!     *'  # msg relesed
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
